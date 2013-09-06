4deb82c812bf126bfd49283411258e46534a5e33
ratopi <ratopi@abwesend.de>
2013-09-06 17:53:42 +0200

    This stupid Allow*Origin ... :-)

--- de.ratopi.f4m2.state.js
+++ de.ratopi.f4m2.state.js
@@ -15,38 +15,6 @@ de.ratopi.f4m2.state = function( directoryURL )
 
 	var directory = {}; // holds the urls of the Freifunk network state objects ...
 
-	directory = {
-                	"augsburg": "http://www.wgaugsburg.de/ffapi/ffapi.json",
-                	"battlemesh" : "http://feeds.leipzig.freifunk.net/ffapi/battlemesh.json",
-                	"berlin" : "http://berlin.freifunk.net/static/berlin.json",
-                	"bielefeld" : "http://vpn1.freifunk-bielefeld.de/freifunk/ffapi.json",
-                	"brilon" : "http://freifunk-brilon.net/map/generator/ffb_api.json",
-                	"chemnitz" : "http://api.routers.chemnitz.freifunk.net/request.php?apikey=f666e95f44c2a54c90f408a2043e30d970fc5f1b&type=com.ffc.info.general",
-                	"dresden" : "http://info.ddmesh.de/info/freifunk.json",
-                	"franken" : "https://raw.github.com/mojoaxel/freifunkfranken-community/master/freifunkfranken.json",
-                	"frankfurt_am_main" : "http://kdserv.dyndns.org/ff-frankfurt.json",
-                	"gadow" : "http://feeds.leipzig.freifunk.net/ffapi/gadow.json",
-                	"gronau" : "http://freifunk.liztv.net/ffapi/gronau.json",
-                	"grossdraxdorf" : "http://feeds.leipzig.freifunk.net/ffapi/grossdraxdorf.json",
-                	"halle" : "http://www.freifunk-halle.net/freifunk-api.json",
-                	"hamburg" : "http://meta.hamburg.freifunk.net/ffhh.json",
-                	"jena" : "http://freifunk-jena.de/jena.json",
-                	"kbu" : "http://map.kbu.freifunk.net/ffapi/ffapi.json",
-                	"kiel" : "http://freifunk.in-kiel.de/ffapi.json",
-                	"leipzig" : "http://feeds.leipzig.freifunk.net/ffapi/leipzig.json",
-                	"leisnig" : "http://feeds.leipzig.freifunk.net/ffapi/leisnig.json",
-                	"luebeck" : "https://luebeck.freifunk.net/ffapi.json",
-                	"lueneburg" : "http://freifunk-lueneburg.de/lueneburg.json",
-                	"potsdam" : "http://freifunk-potsdam.de/ffapi.json",
-                	"rheinland" : "https://freifunk-rheinland.net/ffapi.json",
-                	"rostock" : "http://www.opennet-initiative.de/api.freifunk.net-status.json",
-                	"wahlsdorf": "http://rensky.net/freifunk/wahlsdorf.json",
-                	"weimarnetz" : "http://weimarnetz.de/weimarnetz.json",
-                	"wuppertal" : "http://api.freifunk-wuppertal.net/ffnet/ffwtal.json",
-                	"zootzen" : "http://feeds.leipzig.freifunk.net/ffapi/zootzen.json",
-                	"wiesenburg" : "http://wiki.freifunk.net/images/7/79/Wiesenburg_freifunk_api.json"
-                };
-
 	var states = {}; // holds the states objects of all freifunk networks.  Keys are the ones used in the directory.
 
 	var meta = {}; // holds the row IDs for each Freifunk network in the state table
@@ -71,6 +39,7 @@ de.ratopi.f4m2.state = function( directoryURL )
 			.attr( 'title', null )
 			.removeClass( 'error' )
 			.removeClass( 'info' )
+			.removeClass( 'success' )
 			.hide();
 
 		var cssClass = null;
@@ -81,6 +50,10 @@ de.ratopi.f4m2.state = function( directoryURL )
 		else if ( level === 'info' )
 		{
 			cssClass = 'info';
+		}
+		else if ( level === 'success' )
+		{
+			cssClass = 'success';
 		}
 
 		$( '#state' )
@@ -94,28 +67,36 @@ de.ratopi.f4m2.state = function( directoryURL )
 
 	function updateDirectory()
 	{
-		$.getJSON( directoryURL ).done(
-			function( newDirectoryData )
-			{
-				directory = newDirectoryData;
-			}
-		)
-		.done(
-			function()
-			{
-				setState( "info", "Read directory.json from '" + directoryURL + "'" );
-			}
-		)
-        .fail(
-            function( e )
-            {
-				setState( "error", "Could not get directory.json from '" + directoryURL + "'" );
-            }
-        );
+		$.ajax( {
+			url: directoryURL,
+			type: 'GET',
+			dataType: 'json',
 
-        updateStates();
+			beforeSend:
+				function()
+                {
+                    setState( "info", "Trying to reach '" + directoryURL + "'" );
+                },
 
-		window.setTimeout( updateDirectory, updateInterval );
+			error:
+				function( e )
+				{
+					setState( "error", "Could not get directory.json from '" + directoryURL + "'" );
+				},
+
+			success:
+				function( newDirectoryData )
+				{
+					setState( "success", "Read directory.json from '" + directoryURL + "'" );
+					directory = newDirectoryData;
+					updateStates();
+				},
+			complete:
+				function()
+				{
+					window.setTimeout( updateDirectory, updateInterval );
+				}
+		} );
 	}
 
 	// ---
@@ -127,7 +108,7 @@ de.ratopi.f4m2.state = function( directoryURL )
 		{
 			var rowId = "status_for_" + id;
 
-			$( "#statusTable" ).append( '<tr id="' + rowId + '">' + id + '<td></td><td></td><td></td><td></td><td></td></tr>' );
+			$( "#statusTable" ).append( '<tr id="' + rowId + '"><td>' + id + '</td><td></td><td></td><td></td><td>?</td></tr>' );
 
 			m = { "rowId": rowId, "lastUpdate": 0, "row": $( '#' + rowId ) };
 			meta[ id ] = m;
@@ -138,12 +119,16 @@ de.ratopi.f4m2.state = function( directoryURL )
 
 	function setStateInTable( id, state )
 	{
-        var row = getMetaFor( id ).row;
+		var meta = getMetaFor( id );
+
+		meta.lastUpdate = state.state.lastchange;
+
+        var row = meta.row;
         $( "td:nth-child(1)", row ).text( id );
         $( "td:nth-child(2)", row ).text( state.name );
         $( "td:nth-child(3)", row ).text( state.state.nodes );
         $( "td:nth-child(4)", row ).text( state.state.lastchange );
-        $( "td:nth-child(5)", row ).text( "" ).attr( 'title' );
+        $( "td:nth-child(5)", row ).text( "" ).attr( 'title', null );
         row.removeClass( 'failed' );
 	}
 
